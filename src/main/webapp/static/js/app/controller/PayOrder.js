53c587c3f97e5d5838762a1056a21595b350f0bd
Libo <13843517870@163.com>
2017-05-17 11:22:29 +0800

    增加微信支付功能

--- src/main/webapp/static/js/app/controller/PayOrder.js
+++ src/main/webapp/static/js/app/controller/PayOrder.js
@@ -6,7 +6,10 @@ define([
     var code = base.getUrlParam("code") || "",
         //receiptType = Dict.get("receiptType"),
         // contentTmpl = __inline("../ui/pay-order-imgs.handlebars"),
-        addressTmpl = __inline("../ui/pay-order-address.handlebars");
+        addressTmpl = __inline("../ui/pay-order-address.handlebars"),
+        wxConfig = {},
+        channel ="";
+
     init();
 
     function init() {
@@ -14,9 +17,35 @@ define([
             location.href = "../user/login.html?return=" + base.makeReturnUrl();
         } else {
             queryOrder();
-            addListener();
+            addListeners();
+            // getUserInfo();
         }
     }
+
+    function getUserInfo() {
+        Ajax.get(APIURL + '/user', {})
+            .then(function(response) {
+                $("#loadI").hide();
+                if (response.success) {
+                    var data = response.data;
+                    console.log(data.openId)
+                    var html = "";
+                    if (data.openId == "") {
+                        html+='<div class="wp100 ptb10 plr10 p_r t_323232 s_14 bg_fff" id="wxPay">'+
+                             '<div class="wp100 clearfix" id="wxSbtn">'+
+                             '<img class="fl w60p" src="../../images/wechat.png">'+
+                             '<div class="pl10 fl">'+
+                             '<div class="mtb4">微信账户</div>'+
+                             '<div class="mtb4">微信支付</div>'+
+                             '</div>'+
+                             '<i class="r-tip"></i>'+
+                             ' </div>'+
+                             '</div>';
+                         $("#content").html(html);
+                    } 
+                } 
+            })
+     }       
 
     function queryOrder() {
         var url = APIURL + '/operators/queryOrder',
@@ -69,24 +98,87 @@ define([
     // function getReceiptType(data) {
     //     return data == "" ? "无" : receiptType[data];
     // }
-
-    function addListener() {
-        $("#sbtn").on("click", function(e) {
-            e.stopPropagation();
-            $("#loaddingIcon").removeClass("hidden");
-            Ajax.post(APIURL + '/operators/payOrder', {
-                code: code
-            }).then(function(response) {
-                if (response.success) {
-                    location.href = "./pay_success.html";
+    function onBridgeReady() {
+        WeixinJSBridge.invoke(
+            'getBrandWCPayRequest', {
+                "appId": wxConfig.appId, //公众号名称，由商户传入
+                "timeStamp": wxConfig.timeStamp, //时间戳，自1970年以来的秒数
+                "nonceStr": wxConfig.nonceStr, //随机串
+                "package": wxConfig.wechatPackage,
+                "signType": wxConfig.signType, //微信签名方式：
+                "paySign": wxConfig.paySign //微信签名
+            },
+            function(res) {
+                // loading.hideLoading();
+                // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                if (res.err_msg == "get_brand_wcpay_request:ok") {
+                    base.showMsg("支付成功");
+                    setTimeout(function(){
+                        location.href = "./pay_success.html";
+                    }, 1000);
                 } else {
-                    $("#loaddingIcon").addClass("hidden");
-                    base.showMsg(response.msg);
+                    base.showMsg("支付失败");
                 }
-            }, function() {
+            }
+        );
+    }
+    function wxPay(data){
+        wxConfig = data;
+        if (data && data.signType) {
+            if (typeof WeixinJSBridge == "undefined") {
+                if (document.addEventListener) {
+                    document.removeEventListener("WeixinJSBridgeReady", onBridgeReady);
+                    document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+                } else if (document.attachEvent) {
+                    document.detachEvent('WeixinJSBridgeReady', onBridgeReady);
+                    document.detachEvent('onWeixinJSBridgeReady', onBridgeReady);
+                    document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+                    document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+                }
+            } else {
+                onBridgeReady();
+            }
+        } else {
+            loading.hideLoading();
+            base.showMsg(data.msg || "微信支付失败");
+        }
+    }
+
+    function pay(e,channel) {
+        e.stopPropagation();
+        $("#loaddingIcon").removeClass("hidden");
+        Ajax.post(APIURL + '/operators/payOrder', {
+            code: code,
+            channel: channel
+        }).then(function(response) {
+            if (response.success) {
+                // $("#wxPay").
+                if (channel == "01") {
+                    $("#loaddingIcon").addClass("hidden");
+                    setTimeout(function(){
+                        location.href = "./pay_success.html";
+                    }, 1000);
+                }else{
+                    $("#loaddingIcon").addClass("hidden");
+                    wxPay(response.data);
+                }
+            } else {
                 $("#loaddingIcon").addClass("hidden");
-                base.showMsg("非常抱歉，订单支付失败");
-            });
+                base.showMsg(response.msg);
+            }
+        }, function() {
+            $("#loaddingIcon").addClass("hidden");
+            base.showMsg("非常抱歉，订单支付失败");
         });
     }
+
+    function addListeners() {
+        $("#jfSbtn").on("click", function(e) {
+           pay(e,"01");
+       });
+       $("#wxSbtn").on("click", function(e) {
+           pay(e,"02")
+        });
+    }
+
 });
